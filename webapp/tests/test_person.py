553d3415876ca3ca93b02f0a49fe567532048aab
Chiedo John <cj@chie.do>
2014-06-23 11:06:40 -0400

    Minor

--- webapp/tests/test_person.py
+++ webapp/tests/test_person.py
@@ -4,7 +4,7 @@ from webapp import db
 
 
 class Tests(BaseTestCase):
-    def test_model_create(self):
+    def test_model(self):
         x = Person(name='John Doe', age=25, email='johndoe@gmail.com')
         db.session.add(x)
         db.session.commit()
