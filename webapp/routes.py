c428a39cb54b2220b9061ef0a13285da1f50ad3d
Chiedo John <cj@chie.do>
2014-07-18 10:51:52 -0400

    Static pages controller

--- webapp/routes.py
+++ webapp/routes.py
@@ -8,9 +8,11 @@ def not_found(error):
     return render_template('404.html'), 404
 
 # Import Contollers
+import webapp.controllers.static_pages as static_pages
 import webapp.controllers.index as index
 import webapp.controllers.person as person
 
 # Register blueprints and their routes
+app.register_blueprint(static_pages.routes)
 app.register_blueprint(index.routes)
 app.register_blueprint(person.routes, url_prefix='/person')
