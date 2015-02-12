e505835b2b6820c939bd609da59de0e569c560da
Chiedo <chiedo@chie.do>
2015-02-12 13:41:43 -0500

    Minor

--- project/__init__.py
+++ project/__init__.py
@@ -16,17 +16,20 @@ app.config.from_object('project.config.BaseConfiguration')
 db = SQLAlchemy(app)
 
 # Connect all the routes from the controllers
-controller_names = []
-for file in [doc for doc in os.listdir("project/controllers") if doc.endswith(".py") and doc != "__init__.py"]:
-    controller_names.append("project.controllers." + file.split(".")[0])  # remove the .py from the file name
+try:
+    controller_names = []
+    for file in [doc for doc in os.listdir("project/controllers") if doc.endswith(".py") and doc != "__init__.py"]:
+        controller_names.append("project.controllers." + file.split(".")[0])  # remove the .py from the file name
 
-# Loop through all contollers and register their blueprint
-for controller_name in controller_names:
-    # This dynamically imports all modules in the tests_to_run list. This allows me to import a module using
-    # a variable. This is fairly advanced and hard to follow for the beginner.
-    controller = __import__(controller_name, fromlist=[controller_name])
-    if hasattr(controller, "routes"):
-        app.register_blueprint(controller.routes)
+    # Loop through all contollers and register their blueprint
+    for controller_name in controller_names:
+        # This dynamically imports all modules in the tests_to_run list. This allows me to import a module using
+        # a variable. This is fairly advanced and hard to follow for the beginner.
+        controller = __import__(controller_name, fromlist=[controller_name])
+        if hasattr(controller, "routes"):
+            app.register_blueprint(controller.routes)
+except(OSError):
+    exit()
 
 # Creates all tables not created for each controller that was imported above
 db.create_all()
