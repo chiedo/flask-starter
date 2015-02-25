a93a65aec0ccd322bafa805bd19d24e1d233f621
Chiedo <chiedo@chie.do>
2015-02-25 14:57:36 -0500

    Simplified

--- project/tests/factories.py
+++ project/tests/factories.py
@@ -1,21 +1,14 @@
 import factory
-import factory.alchemy
 import project.models as models
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
 from random import randint
 from faker import Faker
 fake = Faker()
-from project.config import BaseConfiguration
-engine = create_engine(BaseConfiguration.SQLALCHEMY_DATABASE_URI)
-
-session = scoped_session(sessionmaker(bind=engine))
 
 
-class PersonFactory(factory.alchemy.SQLAlchemyModelFactory):
+class PersonFactory(factory.Factory):
+
     class Meta:
         model = models.person.Person
-        sqlalchemy_session = session
 
     name  = fake.name()
     email = fake.email()
